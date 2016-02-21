77a9dd3db942b1ba5fcf0a89e26ce35dc3ed0286
Leo Hemsted <leohemsted@gmail.com>
2016-02-21 23:09:03 +0000

    added selected option

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-console.log('hello');
-
-var load_content = function(event) {
+var selectNavOption = function(event) {
     event.preventDefault();
+
+    document.querySelector('.selected').classList.remove('selected');
+    event.currentTarget.parentElement.classList.add('selected');
+
     var xhr = new XMLHttpRequest();
     xhr.addEventListener("load", function(){
-        console.log('loaded!')
         document.getElementById('main').innerHTML = this.responseText;
     });
-    xhr.open('GET', event.currentTarget.getAttribute('data-content_url'));
+    xhr.open('GET', event.currentTarget.getAttribute('data-content-url'));
     xhr.send();
 };
 
 // get all the links in the navbar with the internal class - we'll want those to trigger our single page app loading shenanigans. The external links we can just ignore
 Array.prototype.slice.call(document.querySelectorAll('.nav a.internal')).map(
     function(x){
-        x.onclick = load_content;
+        x.onclick = selectNavOption;
 });
+
+
+
+// select the first element
+document.querySelector('.nav li').classList.add('selected');
